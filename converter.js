b82ef6bbf004f7f11f896fe277023dc34fbf8b04
mlink <mlinkevic@gmail.com>
2025-03-17 11:19:12 +0100

    First commit

---
+++ converter.js
@@ -0,0 +1,29 @@
+const base_url = 'https://api.apilayer.com/fixer';
+const api_key = 'wjkYpWHO5PomooWzNiVmCg5dCCeXXxJg';
+convert.onclick = () => {
+    fetch(`${base_url}/convert?from=${from.value.trim()}&to=${to.value.trim()}&amount=${sum.value.trim()}`, {
+        headers: {
+            apikey: api_key
+        }
+    })
+        .then(response => response.json())
+        .then(data => data.result)
+        .then(res => {
+            const h1 = document.createElement('h1');
+            h1.append(`Result: ${res.toFixed(2)}`);
+            if (result.firstElementChild) {
+                result.replaceChild(h1, result.firstElementChild);
+            } else {
+                result.append(h1);
+            }
+        })
+        .catch(e => {
+            const h1 = document.createElement('h1');
+            h1.append(`Error`);
+            if (result.firstElementChild) {
+                result.replaceChild(h1, result.firstElementChild);
+            } else {
+                result.append(h1);
+            }
+        })
+}
